7083238a728073a6bfe8c00ea34e29d1bab9c6ba
AKhil Kumar <akhilkumar03456@gmail.com>
2023-12-26 13:03:39 +0530

    Project Zip Files

--- pack1/proAccessSpecifier.java
+++
@@ -1,7 +0,0 @@
-package pack1;
-
-public class proAccessSpecifier {
-	protected void display() {
-		System.out.println("This using protected access specifier");
-	}
-}
--- pack3/pubAccess.java
+++
@@ -1,7 +0,0 @@
-package pack3;
-
-public class pubAccess {
-	public void display() {
-		System.out.println("Using public access Specifier");
-	}
-}
